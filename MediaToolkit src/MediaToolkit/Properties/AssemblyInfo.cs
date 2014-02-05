b04aedc8b0c9b1ecc212d03274353fa1fb842983
Fatih Aydin <faydinv1@gmail.com>
2014-02-05 19:15:50 +0000

    Cleaned up the assembly info.

--- MediaToolkit src/MediaToolkit/Properties/AssemblyInfo.cs
+++ MediaToolkit src/MediaToolkit/Properties/AssemblyInfo.cs
@@ -5,27 +5,15 @@
  * (https://github.com/AydinAdn/MediaToolkit/tree/master/FFMpeg%20win32%20static%20Library%2002-04-2014)
  * **********************/
 
-
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("MediaToolkit")]
 [assembly: AssemblyDescription("A .NET wrapper for FFmpeg, to convert & process all types of media.")]
 [assembly: AssemblyProduct("MediaToolkit")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
-
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("e03e0a90-f6c1-4d13-8649-d61783abeff3")]
-
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
 [assembly: AssemblyCompanyAttribute("Aydin")]
