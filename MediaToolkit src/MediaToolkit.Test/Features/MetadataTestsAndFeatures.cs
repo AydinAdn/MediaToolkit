c8cd70a88ce2f0ecb97a2496e7d2963645733081
Aydin <faydinv1@gmail.com>
2016-02-05 23:22:18 +0000

    New features in the works, grabbing full metadata with exiftool

---
+++ MediaToolkit src/MediaToolkit.Test/Features/MetadataTestsAndFeatures.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using MediaToolkit.Features;
+using MediaToolkit.Util;
+using NUnit.Framework;
+
+namespace MediaToolkit.Test.Features
+{
+    [TestFixture]
+    public class MetadataTestsAndFeatures
+    {
+        [Test]
+        public void Init()
+        {
+            MetadataProvider provider = new MetadataProvider();
+            var meta = provider.GetMetadata(
+                @"../../TestVideo/BigBunny.m4v");
+
+            meta.MetadataIndex.Select(kvp => "{0}:{1}".FormatInvariant(kvp.Key, kvp.Value) )
+                              .ToList()
+                              .ForEach(Console.WriteLine);
+        }
+
+
+    }
+}
---
+++ MediaToolkit src/MediaToolkit/Features/MetadataProvider.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using MediaToolkit.Util;
+
+namespace MediaToolkit.Features
+{
+    public class MetadataProvider : IMetadataProvider
+    {
+        const string DefaultExiftoolPath = "/MediaToolkit/exiftool.exe";
+
+        public MetadataProvider()
+        {
+            Document.Decompress("MediaToolkit.Resources.Exiftool.exe.gz", DefaultExiftoolPath);
+
+            this.isDisposed = false;
+            this.exiftoolProcess = new Process();
+        }
+
+        private bool isDisposed;
+
+        private readonly Process exiftoolProcess;
+
+        protected virtual void Dispose(bool isDisposing)
+        {
+            if (!isDisposing) return;
+            if (this.isDisposed) return;
+
+            this.isDisposed = true;
+            this.exiftoolProcess.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+        }
+
+        public IMetadata GetMetadata(string filename)
+        {
+            exiftoolProcess.StartInfo = new ProcessStartInfo
+            {
+                Arguments = "\"{0}\"".FormatInvariant(filename),
+                FileName = DefaultExiftoolPath,
+                CreateNoWindow = true,
+                RedirectStandardInput = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                WindowStyle = ProcessWindowStyle.Hidden
+            };
+
+            Dictionary<string, string> metadataIndex = new Dictionary<string, string>();
+
+            exiftoolProcess.OutputDataReceived += (sender, args) =>
+            {
+                if (args.Data.Contains("ExifTool Version Number")) return;
+
+                var keyValue = args.Data.Split(':');
+                metadataIndex.Add(keyValue[0], keyValue[1]);
+            };
+
+            exiftoolProcess.Start();
+            exiftoolProcess.BeginOutputReadLine();
+            exiftoolProcess.WaitForExit();
+
+            return new Metadata {MetadataIndex = metadataIndex};
+        }
+    }
+    public interface IMetadataProvider : IDisposable
+    {
+        IMetadata GetMetadata(string filename);
+    }
+
+    public interface IMetadata
+    {
+        Dictionary<string, string> MetadataIndex { get; set; } 
+    }
+
+    public class Metadata : IMetadata
+    {
+        public Dictionary<string, string> MetadataIndex { get; set; }
+    }
+}
