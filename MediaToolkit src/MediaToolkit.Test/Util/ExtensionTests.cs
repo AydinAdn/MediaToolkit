3eefb51ed020673e9894c46d849382e8da2b4262
Aydin <faydinv1@gmail.com>
2016-04-08 04:12:13 +0100

    Added Extension Tests

---
+++ MediaToolkit src/MediaToolkit.Test/Util/ExtensionTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using MediaToolkit.Util;
+using Moq;
+using NUnit.Framework;
+
+namespace MediaToolkit.Test.Util
+{
+    [TestFixture]
+    public class ExtensionTests
+    {
+        public class ForEach
+        {
+            [Test]
+            public void Will_Iterate_Through_EachItem_InCollection()
+            {
+                IEnumerable<string> collectionUnderTest = new[] { "Foo", "Bar" };
+                int expectedIterations = 2;
+                int iterations = 0;
+
+                collectionUnderTest.ForEach(item => iterations++);
+
+                Assert.That(iterations == expectedIterations);
+            }
+        }
+    }
+}
