eb963c52f7390963a332f70ab8568b4375d63bc6
Aydin <AydinAdn@users.noreply.github.com>
2020-01-19 16:57:20 +0300

    Added interface for command handling

---
+++ MediaToolkit src/MediaToolkit.Core/CommandHandler/IHandleCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MediaToolkit.Core.CommandHandler
+{
+    public interface IHandleCommand
+    {
+        Task<bool> HandleAsync(string command, CancellationToken token = default);
+    }
+}
