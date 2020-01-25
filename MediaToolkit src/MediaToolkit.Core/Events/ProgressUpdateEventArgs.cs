5ca7398243b1249aa810bae8f2bb97fde1fbaec8
Aydin <AydinAdn@users.noreply.github.com>
2020-01-25 08:53:27 +0300

    Created EventArgs to be implemented

---
+++ MediaToolkit src/MediaToolkit.Core/Events/ProgressUpdateEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaToolkit.Core.Events
+{
+    public class ProgressUpdateEventArgs : EventArgs
+    {
+        public ProgressUpdateEventArgs()
+        {
+        }
+
+        public ProgressUpdateEventArgs(Dictionary<string, string> updateData)
+        {
+            this.UpdateData = updateData;
+        }
+
+        public Dictionary<string, string> UpdateData { get; set; }
+    }
+
+}
---
+++ MediaToolkit src/MediaToolkit.Core/Events/WarningEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MediaToolkit.Core.Events
+{
+    public class WarningEventArgs : EventArgs
+    {
+        public WarningEventArgs(string warning)
+        {
+            this.Warning = warning;
+        }
+
+        public string Warning { get; set; }
+    }
+}
